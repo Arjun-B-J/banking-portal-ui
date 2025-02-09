7047e067bb6f65c53b6b33ad5e2ea2e09f665298
Arjun BJ <47909735+Arjun-B-J@users.noreply.github.com>
2025-02-09 16:20:39 +0530

    update

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api/api',
+  apiUrl: 'http://bankingbackend-teampheonix123-dev.apps.rm1.0a51.p1.openshiftapps.com/api',
   tokenName :  "authToken",
-  origin:'http://localhost:4200' 
+  origin:'http://bankingbackend-teampheonix123-dev.apps.rm1.0a51.p1.openshiftapps.com' 
 };
